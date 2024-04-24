a4d8f8ddfa658c1a6f59972d6a0043a88df54e82
Alejandro Jara <alejandroejara@hotmail.com>
2024-04-24 13:11:49 -0300

    Ejercicios clase 3

---
+++ Clase3/Ejercicio 2/Biblioteca.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio_2
+{
+    public class Biblioteca
+    {
+        public List<string> Libros { get; set; }
+
+        public Biblioteca()
+        {
+            Libros = new List<string>()
+            {
+                "Cien años de soledad",
+                "1984",
+                "Don Quijote de la Mancha",
+                "Matar a un ruiseñor",
+                "El Gran Gatsby",
+                "Orgullo y prejuicio",
+                "Crónica de una muerte anunciada",
+                "El señor de los anillos",
+                "Los miserables",
+                "La sombra del viento"
+            };
+        }
+        
+
+    }
+}
---
+++ Clase3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Clase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ejercicio 1
+
+
+            // Crear clase Vehiculo con las siguientes propiedades: Marca, Modelo
+            // Crear clase Moto que herede de Vehiculo  
+            // Crear clase Auto que herede de Vehiculo
+
+            //Modificar el metodo "ImprimirDatosVehiculo" para que pueda imprimir los datos del Vehiculo independientemente de que si es una moto o un auto 
+
+            //Uso
+            //ImprimirDatosVehiculo(moto);
+            //ImprimirDatosVehiculo(auto);
+            #endregion
+
+
+            #region Ejercicio 2
+            // Crear una interfaz que se llame IBiblioteca
+            // Definir en la interfaz con una sola firma que indique que se va a obtener todos los libros
+            // Implementar la interfaz en la clase biblioteca para que devuelva los libros
+            // Instanciar y utilizar la clase biblioteca
+            #endregion
+        }
+
+        static void ImprimirDatosVehiculo() 
+        {   
+        }
+
+
+    }
+}
